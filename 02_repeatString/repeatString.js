fae668bbe71af461effe8cdfaaa7fcf556c1d317
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-19 11:45:01 +0800

    feat: add function to repeat strings that passes tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if(num < 0) {
+        return "ERROR";
+    }
 
-};
-
+    let final = '';    
+    while(num > 0) {
+        final += string;
+        num--;
+    }
+    return final;
+}
 // Do not edit below this line
 module.exports = repeatString;
