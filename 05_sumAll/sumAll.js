b925d4a78d88fb23c2d28e41d89525d18ff14239
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-20 03:00:11 +0800

    feat: add function to add numbers between two integers passing certain tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
 
+    let beginNumber = num1;
+    let endNumber = num2;
+    if (num2 < num1) {
+        beginNumber = num2;
+        endNumber = num1;
+    } 
+
+    let sum = 0;
+    for (i = beginNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
