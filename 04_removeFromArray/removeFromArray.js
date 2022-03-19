10251f0d683fd59e85046ba4aa497102f1dfa686
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-20 02:27:04 +0800

    feat: add function to remove an element from an array passing through certain tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function ([...alphanumeric], ...remove) {
+    const removal = Array.of(...remove);
+    let array = [...alphanumeric];
 
+    for (const rmv of removal) {
+
+        if (array.includes(rmv)) {
+            const removedIndex = array.indexOf(rmv);
+            array.splice(removedIndex, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
