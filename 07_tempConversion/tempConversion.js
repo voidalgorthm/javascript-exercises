ae4ea44a3e4d3bec0295738a76225646bb0acbc3
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-20 21:11:30 +0800

    feat: add functions converting temperature from fahrenheit to celcius and vice-versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let converted = (temp - 32) * (5 / 9);
+  let fahr = Math.round(converted * 10) / 10;
+  return fahr;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let converted = (temp * (9 / 5)) + 32;
+  let cels = Math.round(converted * 10) / 10;
+  return cels;
 };
 
 // Do not edit below this line
