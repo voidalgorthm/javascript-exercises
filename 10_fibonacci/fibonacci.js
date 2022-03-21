bd38b5d9a27d44eb2c06f962c87209e3f6f2d637
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-21 23:07:57 +0800

    feat: add function to determine nth-member of fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+    let converted = Number(member);
+    if (converted < 0 || typeof converted !== 'number') {
+        return "OOPS";
+    }
 
+    let pop = 1;
+    let push = 1;
+    let fibo = 0;
+    for (let i = 1; i <= converted; i++) {
+        let stack = pop + push; // 2 3 5
+        fibo = pop; // 1 1 2
+        pop = push; // 1 2 3
+        push = stack; // 2 3 5
+
+        if (i === converted) return fibo;
+    }
 };
 
 // Do not edit below this line
