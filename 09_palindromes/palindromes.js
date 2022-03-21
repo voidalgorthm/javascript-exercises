0b087d02432ca21b4da0f26c0634526b5b7e8a58
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-21 21:49:16 +0800

    feat: add function to determine if a word/phrase is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const word = phrase.toLowerCase().replace(/[^a-zA-Z]/g, "");
+    const length = word.length;
+    const last = length - 1;
+    if (length % 2 == 0) {
+        // even palindromes, odd mid
+        mid = (last - 1) / 2;
+    } else {
+        // odd palindromes, even mid
+        mid = last / 2;
+    }
+
+    for (let start = 0; start <= mid; start++) {
+        if (word.charAt(start) !== word.charAt(last - start)) {
+            return false;
+        }
+
+        if (start === mid || ((mid - start === 1) && ((last - start) - mid) === 1)) {
+            return true;
+        }
+    }
 
 };
 
