c7b610c32f1652b3a8a901fa1f1f62e497956e8f
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-22 21:00:37 +0800

    feat: add function to determine oldest person from an object array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = determineAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const personAge = determineAge(person.yearOfBirth, person.yearOfDeath);
+    return oldestAge < personAge ? person : oldest;
+  })
+}
 
+const determineAge = (yearOfBirth, yearOfDeath) => {
+  if(!yearOfDeath){
+    yearOfDeath = new Date().getFullYear();
+  }
+
+  return yearOfDeath - yearOfBirth;
 };
 
 // Do not edit below this line
