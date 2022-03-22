94185f7f6fc310b20a24f7762553cd2523ffc625
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-03-22 14:07:43 +0800

    feat: add function to get properties of an array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = ([...books]) => {
+  let array = [...books];
+  let bookTitles = [];
+
+  array.forEach(book => {
+    bookTitles.push(book.title);
+  });
+
+  return bookTitles;
 };
 
 // Do not edit below this line
